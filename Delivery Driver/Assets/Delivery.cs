f6e2acaf013a5c47b11d44ad6a2ead815753d97d
jufupu <45634869+jufupu@users.noreply.github.com>
2023-03-30 16:22:47 +0100

    delivery driver update

---
+++ Delivery Driver/Assets/Delivery.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    bool hasPackage;
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Oh god Rmac you've crashed again!!");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+       if (other.tag == "Package")
+       {
+            Debug.Log("Package picked up!");
+            hasPackage = true;
+       }
+       if (other.tag == "Customer" && hasPackage)
+       {
+            Debug.Log("Package Delivered to customer!");
+       }
+    }
+}
